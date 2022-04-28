1d69139e97e6466dc53e443a0bc84784b2d2653b
a906891 <a906891@gmail.com>
2022-04-28 17:34:46 +0530

    added all queries

--- lib/global/queries.dart
+++ lib/global/queries.dart
@@ -1,5 +1,102 @@
 class Queries{
 
+  final String storeCountries = '''
+  query storeCountries(\$page: Int, \$search: String, \$limit: Int, \$sort: String, \$store: ID!) {
+  storeCountries(
+    page: \$page
+    search: \$search
+    limit: \$limit
+    sort: \$sort
+    store: \$store
+  ) {
+    count
+    page
+    pageSize
+    data {
+      id
+      default
+      active
+      country {
+        id
+        name
+        value
+        dialCode
+        code
+        img
+        imgCdn
+        flag
+        lang
+      }
+      user {
+        id
+        firstName
+        lastName
+        email
+      }
+    }
+  }
+}
+  ''';
+
+  final String cities = '''
+  query cities(\$page: Int, \$search: String, \$limit: Int, \$sort: String) {
+  cities(page: \$page, search: \$search, limit: \$limit, sort: \$sort) {
+    count
+    page
+    pageSize
+    data {
+      id
+      name
+      slug
+      country {
+        id
+        name
+      }
+      state {
+        id
+        name
+      }
+      location
+      lat
+      lng
+    }
+  }
+}
+  ''';
+
+
+
+  final String states = '''
+  query states(\$page: Int, \$search: String, \$limit: Int, \$sort: String, \$country: ID) {
+  states(
+    page: \$page
+    search: \$search
+    limit: \$limit
+    sort: \$sort
+    country: \$country
+  ) {
+    count
+    page
+    pageSize
+    data {
+      id
+      name
+      value
+      code
+      img
+      imgCdn
+      flag
+      lang
+      sort
+      active
+      createdAt
+      updatedAt
+    }
+  }
+}
+  ''';
+
+
   final String categories='''
   query categories(\$page: Int, \$search: String, \$limit: Int, \$sort: String, \$megamenu: Boolean, \$featured: Boolean, \$active: Boolean, \$img: Boolean, \$shopbycategory: Boolean, \$store: ID) {
   categories(
@@ -47,5 +144,39 @@ class Queries{
   }
 }
 ''';
+
+  final String amenities ='''
+  query amenities(\$page: Int, \$search: String, \$limit: Int, \$sort: String, \$store: ID) {
+  amenities(
+    page: \$page
+    search: \$search
+    limit: \$limit
+    sort: \$sort
+    store: \$store
+  ) {
+    count
+    page
+    pageSize
+    data {
+      id
+      name
+      icon
+      user {
+        id
+        firstName
+        lastName
+        email
+        phone
+      }
+      store {
+        id
+        name
+      }
+    }
+  }
+}
+''';
+
+
 
 }
