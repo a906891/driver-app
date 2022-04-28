87eda16eb9ed2d8225ef8089121a0b613d1642ee
a906891 <a906891@gmail.com>
2022-04-28 17:10:22 +0530

    form service created

--- lib/tabPages/form/form_service.dart
+++ lib/tabPages/form/form_service.dart
@@ -1,8 +1,21 @@
 
 
 import 'package:driver_app/global/base_service.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import '../../global/queries.dart';
 
 class FormService extends BaseService with Queries{
 
+  Future getAllcategories() async{
+    var result = await client.query(QueryOptions(document: gql(categories)));
+    if(result.hasException){
+      print(result.exception?.graphqlErrors.toList());
+      print(result.exception?.linkException.toString());
+      throw Exception('Internal Error');
+    }
+    else{
+      return result.data!['categories'];
+    }
 
+  }
 }
