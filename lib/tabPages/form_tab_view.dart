17152c2da77e65a0c6b1cd3283ebc8da1f14d4ce
a906891 <a906891@gmail.com>
2022-04-29 18:12:24 +0530

    store Model added

--- lib/tabPages/form_tab_view.dart
+++ lib/tabPages/form_tab_view.dart
@@ -33,6 +33,15 @@ class _FormTabPageState extends State<FormTabPage> {
   FormService formService = FormService();
   StoreCountriesModel storeCountriesModel = StoreCountriesModel();
 
+  String dropdownvalue = 'Item 1';
+
+  var items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
 
   @override
   void initState() {
@@ -54,8 +63,68 @@ class _FormTabPageState extends State<FormTabPage> {
 
             // Countires come here
 
+            DropdownButton(
+              // Initial Value
+              value: dropdownvalue,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),
+              // Array list of items
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
 
+            DropdownButton(
+              // Initial Value
+              value: dropdownvalue,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),
+              // Array list of items
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
 
+            DropdownButton(
+              // Initial Value
+              value: dropdownvalue,
+              // Down Arrow Icon
+              icon: const Icon(Icons.keyboard_arrow_down),
+              // Array list of items
+              items: items.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(),
+              // After selecting the desired option,it will
+              // change button value to selected value
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownvalue = newValue!;
+                });
+              },
+            ),
 
 
 //Name
